8e91bdace7992a9f603b46ab0e95ffb46b8ba494
Societaltrends <app.scieppan@gmail.com>
2018-09-13 11:30:54 +0900

    Chapter 8

---
+++ 8/MyPoint.java
@@ -0,0 +1,22 @@
+//MyPointクラス
+class MyPoint{
+		int x; //X座標
+		int y; //y座標
+
+		void setX(int px){
+				//X座標を設定する
+				x = px;
+		}
+		void setY(int py){
+				//Y座標を設定する
+				y = py;
+		}
+		int getX(){
+				//X座標を得る
+				return x;
+		}
+		int getY(){
+				//Y座標を得る
+				return y;
+		}
+}
