3e085ff8b63dbae7da3032879e8586427c03a85e
app.scieppan <app.scieppan@gmail.com>
2020-02-22 15:31:20 +0900

    20200222

---
+++ Sample11/Sample8.java
@@ -0,0 +1,81 @@
+class Car{
+    protected int num;
+    protected double gas;
+
+    public Car(){
+        num = 0;
+        gas = 0.0;
+        System.out.println("車を作成しました。");
+    }
+
+    public Car(int n, double g){
+        num = n;
+        gas = g;
+        System.out.println("ナンバー"+ num +"ガソリン量"+ gas +"の車を作成しました。");
+    }
+
+    public void setCar(int n, double g){
+        num = n;
+        gas = g;
+        System.out.println("ナンバーを"+ num +"にガソリン量を"+ gas +"にしました。");
+    }
+
+    public void show(){
+        System.out.println("車のナンバーは"+ num +"です。");
+        System.out.println("ガソリン量は"+ gas +"です。");
+    }
+
+    public String toString(){
+        String str = "ナンバー:"+ num +"ガソリン量："+ gas;
+        return str;
+    }
+}
+
+class RacingCar extends Car{
+    private int course;
+
+    public RacingCar(){
+        course = 0;
+        System.out.println("レーシングカーを作成しました。");
+    }
+
+    public RacingCar(int n, double g, int c){
+        super(n, g);
+        course = c;
+        System.out.println("コース番号"+ course +"のレーシングカーを作成しました。");
+    }
+
+    public void setCourse(int c){
+        course = c;
+        System.out.println("コース番号を"+ course +"にしました。");
+    }
+
+    public void show(){
+        System.out.println("レーシングカーのナンバーは"+ num +"です。");
+        System.out.println("ガソリン量は"+ gas +"です。");
+        System.out.println("コース番号は"+ course +"です。");
+    }
+
+    public String toString(){
+        String str = "ナンバー:"+ num +"ガソリン量："+ gas;
+        return str;
+    }
+}
+
+class Sample8
+{
+    public static void main(String[] args){
+        
+       Car car1 = new Car();
+       Car car2 = new Car();
+
+       Car car3;
+       car3 = car1;
+
+       boolean bl1 = car1.equals(car2);
+       boolean bl2 = car1.equals(car3);
+
+       System.out.println("car1とcar2が同じかどうか調べたところ"+ bl1 +"でした。");
+       System.out.println("car1とcar3が同じかどうか調べたところ"+ bl2 +"でした。");
+    }
+}
