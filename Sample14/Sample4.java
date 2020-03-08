36f2a9da4ae6f95f9694f3f7ad4f60c0d2308021
app.scieppan <app.scieppan@gmail.com>
2020-03-08 23:59:24 +0900

    20200308

---
+++ Sample14/Sample4.java
@@ -0,0 +1,19 @@
+class Sample4{
+    public static void main(String[] args){
+        try{
+
+            int[] test;
+            test = new int[5];
+            System.out.println("test[10]に値を代入します。");
+            test[10] = 80;
+            System.out.println("test[10]に80を代入しました。");
+            System.out.println("無事終了しました。");
+    
+        }catch(ArrayIndexOutOfBoundsException e){
+            System.out.println("配列の要素を超えています。\n"+ e +"という例外が発生しました");
+        }finally{
+            System.out.println("最後に必ずこの処理をします。");
+        }
+        System.out.println("無事終了しました。");
+    }
+}
